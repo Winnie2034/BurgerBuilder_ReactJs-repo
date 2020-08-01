21f4f05318189b8bdd49f785f105a8be94720c2c
Winnie2034 <winnerwalecha1998@gmail.com>
2020-08-01 20:31:57 +0530

    Added Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,13 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 
     // constructor(props) {
@@ -18,14 +25,55 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const newCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = { ...this.state.ingredients };
+
+        updatedIngredients[type] = newCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice + priceAddition;
+
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
+
+
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+            if(oldCount <= 0){
+                return;
+            }
+
+        const newCount = oldCount - 1;
+
+        const updatedIngredients = { ...this.state.ingredients };
+
+        updatedIngredients[type] = newCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice - priceDeduction;
+
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls
+                ingredientRemoved={this.removeIngredientHandler} 
+                ingredientAdded={this.addIngredientHandler} />
             </Aux>
 
         );
